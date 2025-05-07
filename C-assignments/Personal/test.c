da32ad816716bc7139417a4d5e44d04b161558b2
Qcid <sengforlinux@gmail.com>
2025-05-07 16:34:57 +0930

    Pushed all assignments to this repository

---
+++ C-assignments/Personal/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+// Compile with gcc {FILE_NAME1}.c -o {FILE_NAME2}
+// Run code with ./{FILE_NAME2}
+
+int main(void) {
+
+    /* Hypotenuse 
+    double a, b;
+    printf("Enter your a value\n");
+    scanf("%lf", &a);
+    printf("Enter your b value\n");
+    scanf("%lf", &b);
+
+    double c1 =  (a * a) + (b * b);
+    double c2 = sqrt(c1);
+
+    printf("Your a: %.2f, Your b: %.2f, Your hypotenuse (c): %.2f", a, b, c2);
+    return 0;
+    */
+}
--- Matlab-assignments/INFO.md
+++ Matlab-assignments/INFO.md
@@ -1,4 +1,4 @@
-# Matlab
-# Date: May 03 2025
+# Matlab Language
+# Date: Mar 03 2025
 
 This folder shows practical assignments from week1 to week7 of University practicals for ENG1002
