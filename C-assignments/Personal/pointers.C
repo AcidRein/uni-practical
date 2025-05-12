6f044f73995f9efdb240c7830b4105ff66f5490c
Seng <sengforschool@example.com>
2025-05-12 13:23:19 +0930

    m

---
+++ C-assignments/Personal/pointers.C
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    int list[3] = {1, 3, 2};
+
+    {1, 2, 3}
+    {2, 3, 1}
+}
---
+++ C-assignments/week9practice/odd_evens.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    int nums[] = {11, 3, 9, 7, 6, 10, 13, 17, 2, 8};
+    int numSize = sizeof(nums) / sizeof(nums[0]);
+    int
+    for (int i = 0; i < numSize; i++) {
+
+    }
+}
