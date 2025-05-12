c34a51d28e16401d780e05b02595c5faec5b00e7
Sengphachanh Phankhounnivongsa <a1967663@emb108w053.ad.adelaide.edu.au>
2025-05-12 15:01:07 +0930

    m

--- C-assignments/week9practice/odd_evens.c
+++ C-assignments/week9practice/odd_evens.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 
-int main(void){
-    int nums[] = {11, 3, 9, 7, 6, 10, 13, 17, 2, 8};
-    int numSize = sizeof(nums) / sizeof(nums[0]);
-    int
-    for (int i = 0; i < numSize; i++) {
+void printVector(int vectorParam[], int vectorSize) {
+  for (int i = 0; i < (vectorSize - 1); i++) {
+    printf("%d ", vectorParam[i]);
+  }
+  printf("%d\n", vectorParam[vectorSize - 1]);
+}
 
+int main(void) {
+  int nums[] = {11, 3, 9, 7, 6, 10, 13, 17, 2, 8, 3};
+  int numSize = sizeof(nums) / sizeof(nums[0]);
+  printf("Vector sIZE: %d\n", numSize);
+
+  int evenNums[11], oddNums[11];
+
+  int evenIndex = 0, oddIndex = 0;
+  for (int i = 0; i < numSize; i++) {
+    if (nums[i] % 2 == 0) {
+      evenNums[evenIndex] = nums[i];
+      evenIndex++;
+    } else {
+      oddNums[oddIndex] = nums[i];
+      oddIndex++;
     }
+  }
+
+  printVector(oddNums, oddIndex);
+  printVector(evenNums, evenIndex);
 }
---
+++ C-assignments/week9practice/process_ages.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main(void) {
+  // Initialize
+  int userAgeInput, userAgeIndex = 0;
+  int minAge = __INT_MAX__, maxAge = -__INT_MAX__, ageSum = 0;
+  int returnValue = 0;
+  char buffer;
+
+  while (1) {
+    // Prompt for Input
+    printf("Enter your age [%d]: ", userAgeIndex);
+    returnValue = scanf("%d", &userAgeInput);
+
+    if (returnValue != 1) {
+      printf("Enter a valid number!\n");
+      scanf(" %c", &buffer);
+      continue;
+    }
+
+    // If user enters -1 or entered 10 people, exit
+    if (userAgeInput == -1 || userAgeIndex >= 9) {
+      break;
+
+      // If user types a negative number or exceeds 150, reprompt
+    } else if (userAgeInput < 0 || userAgeInput > 150) {
+      printf("[%d] is not a valid Age! Try again\n", userAgeInput);
+      continue;
+    }
+
+    // Valid entries
+    userAgeIndex++;
+
+    // Find min/max age
+    if (userAgeInput > maxAge) {
+      maxAge = userAgeInput;
+    } else if (userAgeInput < minAge) {
+      minAge = userAgeInput;
+    }
+
+    // Sum age
+    ageSum = ageSum + userAgeInput;
+  }
+
+  float averageAge = ageSum / userAgeIndex;
+  printf("\nMaximum: %d\nMinimum: %d\nAverage: %.1f\n", maxAge, minAge,
+         averageAge);
+
+  return 0;
+}
